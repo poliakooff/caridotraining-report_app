dfa5a75894d2ed7ca2d2d72cebad9ae582e1f65d
Bohdan <redliz.redliz@gmail.com>
2023-09-04 14:52:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--temp');
+const inputElevation = document.querySelector('.form__input--climb');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com.ua/maps/@${latitude},${longitude},14z?entry=ttu`
+      );
+    },
+    function () {
+      alert('Unable to get your location!');
+    }
+  );
