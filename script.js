bf512e6e3f17d3b97f20a0c34a3293777cee5123
Bohdan <redliz.redliz@gmail.com>
2023-09-07 12:52:05 +0300

    New classes have been added

--- script.js
+++ script.js
@@ -8,6 +8,46 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputTemp = document.querySelector('.form__input--temp');
 const inputClimb = document.querySelector('.form__input--climb');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance; // km
+    this.duration = duration; // min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, temp) {
+    super(coords, distance, duration);
+    this.temp = temp;
+    this.calculatePace();
+  }
+
+  calculatePace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, climb) {
+    super(coords, distance, duration);
+    this.climb = climb;
+    this.calculateSpeed();
+  }
+
+  calculateSpeed() {
+    // km/h
+    this.speed = this.distance / this.duration / 60;
+  }
+}
+
+const running = new Running([50, 39], 7, 40, 170);
+const cycling = new Cycling([50, 39], 37, 80, 370);
+console.log(running, cycling);
+
 class App {
   #map;
   #mapEvent;
